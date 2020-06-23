682be0a9a2f034f101e92bc54d98e8cb7743e41c
rschwarzler <rory.schwarzler@kpn.com>
2020-06-23 08:14:45 +0200

    feat(content): Adding chuck norris ipsum

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,13 +2,15 @@
   <div class="hello">
     <img alt="Vue logo" src="../assets/logo.png" />
     <h1>{{ msg }}</h1>
-    <h3>Cat Ipsum</h3>
+    <h3>Chuck Norris Ipsum</h3>
     <p>
-      Plays league of legends stare out the window. Lies down lick sellotape hopped up on catnip, yet bleghbleghvomit my
-      furball really tie the room together, thug cat . Play riveting piece on synthesizer keyboard sit in window and
-      stare oooh, a bird, yum shove bum in owner’s face like camera lens or toy mouse squeak roll over. Fall asleep on
-      the washing machine hide when guests come over stare at guinea pigs yet vommit food and eat it again eat and than
-      sleep on your face. Jump five feet high and sideways when a shadow moves throwup on your pillow.
+      Chuck Norris can slam a revolving door. Chuck Norris can have his cake and eat it, too. Chuck Norris is currently
+      suing NBC, claiming Law and Order are trademarked names for his left and right legs Chuck Norris can slice meat so
+      thin is only has one side, When Chuck Norris sends in his taxes, he sends blank forms and includes only a picture
+      of himself, crouched and ready to attack. Chuck Norris has not had to pay taxes, ever Chuck Norris is the only man
+      to ever defeat a brick wall in a game of tennis. Chuck Norris looks gift horses in the mouth. Chuck Norris doesn’t
+      wear a watch. HE decides what time it is, The Great Wall of China was originally created to keep Chuck Norris out.
+      It failed miserably.
     </p>
   </div>
 </template>
