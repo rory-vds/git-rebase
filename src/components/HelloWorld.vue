27feaf7b07b06f1f158a9d6ea755b647d7793768
rschwarzler <rory.schwarzler@kpn.com>
2020-06-23 09:30:37 +0200

    feat(test): added unit-test
    feat(content): Adding cheese ipsum

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,16 +2,9 @@
   <div class="hellow">
     <img alt="Vue logo" src="../assets/logo.png" />
     <h1>{{ msg }}</h1>
-    <h3>Lorem Ipsum</h3>
+    <h3>Cheese Ipsum</h3>
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet neque vitae nisl lobortis ullamcorper.
-      Cras ornare malesuada tellus eu sodales. Curabitur dictum metus mollis rhoncus pharetra. Curabitur pellentesque mi
-      et porttitor placerat. Nullam vestibulum feugiat ex, vel porta velit hendrerit pulvinar. Proin vestibulum, ipsum
-      et eleifend suscipit, justo quam blandit sem, id pellentesque augue dui interdum eros. Phasellus vitae orci
-      sagittis, dignissim dui id, lobortis erat. Phasellus interdum ex quis feugiat accumsan. Praesent porttitor iaculis
-      sem, et consectetur tellus ultrices eget. Phasellus eget sagittis nisl. Cras non sagittis lacus. Integer non
-      maximus eros. Mauris mollis, felis nec molestie ullamcorper, augue tortor rutrum augue, nec ullamcorper mauris
-      risus non odio. In auctor finibus quam at viverra.
+      Cheese slices brie cut the cheese. Red leicester caerphilly swiss who moved my cheese manchego cauliflower cheese when the cheese comes out everybody's happy parmesan. Cheeseburger st. agur blue cheese fromage frais emmental the big cheese smelly cheese stilton edam. Fromage say cheese st. agur blue cheese macaroni cheese danish fontina bavarian bergkase fromage frais lancashire. Pepper jack fondue gouda.
     </p>
   </div>
 </template>
